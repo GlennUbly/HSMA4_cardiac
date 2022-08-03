a3f08b9eadb164007df77320144ab1862ec0b994
GlennUbly <88552377+GlennUbly@users.noreply.github.com>
2022-08-03 13:29:54 +0100

    Add files via upload
    Add conclusions file

---
+++ pages/08_Conclusions.py
@@ -0,0 +1,13 @@
+# Page for showing optimal configurations for 1 or 2 sites
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import geopandas as gpd
+import numpy as np
+import seaborn as sbn
+
+'To add'
+st.write('Show optimal configurations for 1 or 2 sites')
+
+st.write('Maybe radio buttons to flip between 1 and 2')
+
