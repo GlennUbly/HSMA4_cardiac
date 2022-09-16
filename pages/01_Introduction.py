790f8a78a8ea6e8cd0036b73fc8f842bae917006
Victor Yu <victoryu@fastmail.com>
2022-09-16 12:30:42 +0100

    Update 01_Introduction.py

--- pages/01_Introduction.py
+++ pages/01_Introduction.py
@@ -4,10 +4,12 @@ import streamlit as st
 
 st.title('Reducing Travel Times to Treatment for Cardiac Patients in South East England')
 
-st.markdown('_Glenn Ubly - NHS England and Improvement<br>Janine James - NHS England and Improvement<br>Antonia Drummond - NHS England and Improvement<br>Victor Yu - Hertfordshire County Council_', unsafe_allow_html=True)
+st.markdown('Done as part of the [HSMA4 programme](https://sites.google.com/nihr.ac.uk/hsma).')
+
+st.markdown('_Glenn Ubly - NHS England and Improvement<br>Janine James - NHS England and Improvement<br>Antonia Drummond - NHS England and Improvement<br>Victor Yu - Hertfordshire County Council_<br>_Adam Scull - Mentor_', unsafe_allow_html=True)
 
 st.markdown('"Moving care closer to home" has become a high-priority goal for many NHS commissioning bodies, in particular for the South East Specialised Commissioning region, with a specific focus on moving cardiac activity away from London and back in to the South East region for all type of referral pathways.')
 
-st.markdown('Bringing care closer to home can reduce travel times for patients, with it being a critical factor in many aspects of healthcare delivery, patient satisfaction and health outcomes.')
+st.markdown('Bringing care closer to home can reduce travel times for patients, with it being a critical factor in aspects of healthcare delivery, patient satisfaction, health outcomes and more.')
 
 st.markdown('Initial scoping of our project focused on all cardiac activity, but we later honed this specifically towards cardiac valve procedures and the Kent and Medway region.')
